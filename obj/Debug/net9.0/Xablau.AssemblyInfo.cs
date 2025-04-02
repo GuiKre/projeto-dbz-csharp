1f892d9673a62193afe36739727cb9a0645e2267
Guilherme Krelling <guilhermekre@gmail.com>
2025-04-02 19:55:33 -0300

    adiciona metodo GET

--- obj/Debug/net9.0/Xablau.AssemblyInfo.cs
+++ obj/Debug/net9.0/Xablau.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Xablau")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5963847398894ae3cb585c117e666374ede0beb4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46e89adabcdfa2b5e158a5f137cb3fa3c5a738d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Xablau")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Xablau")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
