39bd3cf5a75b95aebe4c9e35b9d31f79b6c95aef
Guilherme Krelling <guilhermekre@gmail.com>
2025-04-02 19:57:54 -0300

    crud completo

--- Controllers/PersonagemController.cs
+++ Controllers/PersonagemController.cs
@@ -1,8 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Xablau.Data;
+using Xablau.Models;
 
 namespace Xablau.Controllers
 {
@@ -10,6 +9,31 @@ namespace Xablau.Controllers
     [Route("api/[controller]")]
     public class PersonagemController : ControllerBase
     {
-        
+        private readonly AppDbContext _appDbContext;
+
+        public PersonagemController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        [HttpPost]
+        public async Task <IActionResult> AddPersonagem(Personagem personagem)
+        {
+            if (personagem == null) {
+                return BadRequest("Dados inválidos!");
+            }
+
+            _appDbContext.XablauDB.Add(personagem);
+            await _appDbContext.SaveChangesAsync();
+
+            return StatusCode(201, personagem);
+        }
+        [HttpGet]
+        public async Task <ActionResult <IEnumerable<Personagem>>> GetPersonagem()
+        {
+            var personagens = await _appDbContext.XablauDB.ToListAsync();
+
+            return Ok(personagens);
+        }
     }
 }
