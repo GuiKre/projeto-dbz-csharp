9754ff6005b14e07466dc9542f0c40cfbce31754
Guilherme Krelling <guilhermekre@gmail.com>
2025-04-23 18:41:41 -0300

    script CEP

--- front-end/script.js
+++ front-end/script.js
@@ -1,10 +1,25 @@
 const nome = document.getElementById('nome');
+const form = document.getElementById('form');
+const inputCep = document.getElementById('cep');
 
-const fetchAPI = fetch('https://pokeapi.co/api/v2/pokemon/pikachu')
-    .then((response) => response.json())
-    .then((data) => {
-        // console.log(data);
-        nome.innerText = `${data.name}`
-    });
+const fetchAPI = async (endpoint) => {
+    try {
+        const response = await fetch(endpoint, );
+        const data = await response.json();
+        console.log(data); // Aqui você vê os dados de verdade
 
-// console.log(fetchAPI)
+        if(data.logradouro === '') {
+            nome.innerText = 'Essa cidade é muito boa!';
+        } else {
+            nome.innerText = `${data.logradouro}`;
+        }
+
+    } catch (error) {
+        console.error('Erro:', error);
+    }
+}
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    fetchAPI(`https://viacep.com.br/ws/${inputCep.value}/json`);
+})
